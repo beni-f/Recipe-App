83311dcc77609768ae52dd6bb14c27cfcaa63ecc
beni-f <benifissha@gmail.com>
2024-07-21 16:23:33 +0300

    CRUD funcitonality

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ListRecipes, CreateRecipe
+from .views import ListRecipes, CreateRecipe, DetailRecipe, UpdateRecipe, DeleteRecipe
 urlpatterns = [
    path('', ListRecipes.as_view(), name='recipes'),
-   path('new/', CreateRecipe.as_view(), name='create_recipe')
+   path('new/', CreateRecipe.as_view(), name='create_recipe'),
+   path('<int:pk>/', DetailRecipe.as_view(), name='recipe_detail'),
+   path('<int:pk>/update/', UpdateRecipe.as_view(), name='recipe_update'),
+   path('<int:pk>/delete/', DeleteRecipe.as_view(), name='recipe_delete')
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Recipe
 
 
@@ -17,3 +17,23 @@ class CreateRecipe(CreateView):
     model = Recipe
     fields = ('__all__')
     template_name = 'create_recpie.html'
+
+class DetailRecipe(DetailView):
+    """
+        Displays the details for a recipe
+    """
+    model = Recipe
+    template_name = 'recipe_detail.html'
+
+class UpdateRecipe(UpdateView):
+    """
+        Updates a Recipe
+    """
+    model = Recipe
+    fields = ('__all__')
+    template_name = 'recipe_update.html'
+
+class DeleteRecipe(DeleteView):
+    model = Recipe
+    success_url = 'recipes'
+    template_name = 'recipe_delete.html'
